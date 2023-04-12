e80b4e6b7928fe896eb01b53bdb1ec1984ab5226
LucasGazetta <gazetinhagames@gmail.com>
2023-04-12 18:29:39 -0300

    adicionados components task fork e task list

--- todo-frontend/src/app/app.module.ts
+++ todo-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
